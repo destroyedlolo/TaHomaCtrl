fdbe33664de79f02d76bb6804f53f724d52798b3
destroyedlolo <l_faillie@yahoo.com>
2025-11-09 00:45:38 +0100

    WiP

---
+++ Config.h
@@ -0,0 +1,17 @@
+/* Config.h
+ *
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <stddef.h>
+
+	/* Shared configuration */
+extern const char *tahoma;	/* Tahoma's hostname */
+extern const char *token;	/* Bearer Token */
+
+extern const char *url;	/* base API url */
+size_t url_len;			/* URL's length */
+
+#endif
---
+++ TaHomaCtl.c
@@ -0,0 +1,21 @@
+/* TaHomaCtrl
+ *
+ * CLI tool to control your TaHoma
+ *
+ * History:
+ * 	8/11/2025 - LF - First version
+ */
+
+#include "Config.h"
+
+	/* **
+	 * Configuration
+	 * **/
+const char *tahoma = NULL;
+const char *token = NULL;
+
+const char *url;
+
+int main(int ac, char **av){
+
+}
