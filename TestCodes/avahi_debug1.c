681dc28b6c349893c12dac142fec9ec25065b49d
destroyedlolo <l_faillie@yahoo.com>
2025-11-15 03:24:25 +0100

    Add error messages

--- TestCodes/avahi_debug1.c
+++ TestCodes/avahi_debug1.c
@@ -1,9 +1,3 @@
-/* Trace Avahi messages
- *
- * Compilation : 
-gcc avahi_debug.c -o avahi_debug $(pkg-config --cflags --libs avahi-client glib-2.0) -lc
- */
-
 /*
  * avahi_debug.c
  *
@@ -14,44 +8,49 @@ gcc avahi_debug.c -o avahi_debug $(pkg-config --cflags --libs avahi-client glib-
  *   ./avahi_debug <service-type> <domain>
  * Example:
  *   ./avahi_debug _http._tcp local
+ * Compilation : 
+ * 	gcc -std=c11 -Wall -O2 avahi_debug1.c -o avahi_debug1   $(pkg-config --cflags --libs avahi-client)
+ */
+
+/*
+ * Avahi Debug Browser + Resolver
+ * Compatible Arch Linux (Avahi 0.8)
+ *
+ * Usage:
+ *   ./avahi_debug1 _http._tcp local
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <sys/time.h>
 
 #include <avahi-client/client.h>
 #include <avahi-client/lookup.h>
-#include <avahi-common/error.h>
 #include <avahi-common/simple-watch.h>
-#include <avahi-common/alternative.h>
-#include <avahi-common/malloc.h>
 #include <avahi-common/strlst.h>
 #include <avahi-common/address.h>
+#include <avahi-common/error.h>   /* <-- Needed for avahi_strerror() */
 
-static AvahiSimplePoll *simple_poll = NULL;
-static AvahiClient *client = NULL;
-
-/* Return current time formatted as HH:MM:SS:ms */
-static void timestamp_now(char *buf, size_t buflen)
-{
+/* --------------------------------------------------------- */
+/* Timestamp HH:MM:SS:ms                                     */
+/* --------------------------------------------------------- */
+static void timestamp_now(char *buf, size_t len) {
     struct timespec ts;
-    struct tm tm;
-
     clock_gettime(CLOCK_REALTIME, &ts);
+
+    struct tm tm;
     localtime_r(&ts.tv_sec, &tm);
 
-    int ms = (int)(ts.tv_nsec / 1000000);
-
-    snprintf(buf, buflen, "%02d:%02d:%02d:%03d",
-             tm.tm_hour, tm.tm_min, tm.tm_sec, ms);
+    snprintf(buf, len, "%02d:%02d:%02d:%03ld",
+             tm.tm_hour, tm.tm_min, tm.tm_sec,
+             ts.tv_nsec / 1000000);
 }
 
-/* Convert TXT record list to a readable string */
-static char* txt_to_string(AvahiStringList *txt)
-{
+/* --------------------------------------------------------- */
+/* Convert TXT record to key=value list                      */
+/* --------------------------------------------------------- */
+static char* txt_to_string(AvahiStringList *txt) {
     if (!txt) return NULL;
 
     AvahiStringList *p = txt;
@@ -63,18 +62,14 @@ static char* txt_to_string(AvahiStringList *txt)
         char *value = NULL;
         size_t value_len = 0;
 
-        /* Avahi 0.8 API */
         avahi_string_list_get_pair(p, &key, &value, &value_len);
 
         if (key) {
-            size_t need = strlen(key)
-                        + (value ? value_len + 1 : 0)
-                        + 4;
-
+            size_t need = strlen(key) + (value ? value_len + 1 : 0) + 4;
             char *tmp = realloc(s, len + need);
             if (!tmp) {
-                avahi_free(key);
-                if (value) avahi_free(value);
+                free(key);
+                if (value) free(value);
                 free(s);
                 return NULL;
             }
@@ -98,8 +93,8 @@ static char* txt_to_string(AvahiStringList *txt)
                 s[len] = '\0';
             }
 
-            avahi_free(key);
-            if (value) avahi_free(value);
+            free(key);
+            if (value) free(value);
         }
 
         p = avahi_string_list_get_next(p);
@@ -108,7 +103,9 @@ static char* txt_to_string(AvahiStringList *txt)
     return s;
 }
 
-/* Resolver callback */
+/* --------------------------------------------------------- */
+/* Service Resolver callback                                 */
+/* --------------------------------------------------------- */
 static void resolve_callback(
     AvahiServiceResolver *r,
     AvahiIfIndex interface,
@@ -124,33 +121,53 @@ static void resolve_callback(
     AvahiLookupResultFlags flags,
     void* userdata)
 {
-    (void)interface; (void)protocol; (void)flags; (void)userdata;
+    (void)userdata;
+    (void)interface;
+    (void)protocol;
+    (void)flags;
 
     char ts[16];
     timestamp_now(ts, sizeof(ts));
 
-    if (event == AVAHI_RESOLVER_FOUND) {
-        char addr_str[AVAHI_ADDRESS_STR_MAX];
-        avahi_address_snprint(addr_str, sizeof(addr_str), address);
+    AvahiClient *client = avahi_service_resolver_get_client(r);
 
-        char *txts = txt_to_string(txt);
+    switch (event) {
+    case AVAHI_RESOLVER_FOUND: {
+        char addr[AVAHI_ADDRESS_STR_MAX];
+        avahi_address_snprint(addr, sizeof(addr), address);
 
-        printf("%s RESOLVED: '%s' type='%s' domain='%s'\n",
+        char *txt_str = txt_to_string(txt);
+
+        printf("%s RESOLVED: name='%s' type='%s' domain='%s'\n",
                ts, name, type, domain);
-        printf("    host='%s' addr=%s port=%u\n", host_name, addr_str, port);
-        printf("    txt='%s'\n", txts ? txts : "(none)");
-
-        free(txts);
+        printf("    host='%s' addr=%s port=%u\n",
+               host_name, addr, port);
+        if (txt_str) {
+            printf("    txt='%s'\n", txt_str);
+            free(txt_str);
+        }
+        break;
     }
-    else {
-        printf("%s RESOLVE_FAILED: name='%s' type='%s'\n",
-               ts, name, type);
+
+    case AVAHI_RESOLVER_FAILURE: {
+        int err = avahi_client_errno(client);
+        const char *errstr = avahi_strerror(err);
+        printf("%s RESOLVE_FAILED: name='%s' type='%s' domain='%s' -> %s\n",
+               ts,
+               name ? name : "(null)",
+               type ? type : "(null)",
+               domain ? domain : "(null)",
+               errstr ? errstr : "(unknown error)");
+        break;
+    }
     }
 
     avahi_service_resolver_free(r);
 }
 
-/* Browser callback */
+/* --------------------------------------------------------- */
+/* Service Browser callback                                  */
+/* --------------------------------------------------------- */
 static void browse_callback(
     AvahiServiceBrowser *b,
     AvahiIfIndex interface,
@@ -160,28 +177,25 @@ static void browse_callback(
     const char *type,
     const char *domain,
     AvahiLookupResultFlags flags,
-    void* userdata)
+    void *userdata)
 {
-    (void)b; (void)interface; (void)protocol; (void)flags; (void)userdata;
+    (void)b;
+    (void)flags;
 
     char ts[16];
     timestamp_now(ts, sizeof(ts));
 
-    switch (event) {
+    AvahiClient *client = (AvahiClient*)userdata;
 
+    switch (event) {
     case AVAHI_BROWSER_NEW:
         printf("%s NEW: '%s' type='%s' domain='%s'\n",
                ts, name, type, domain);
-
-        if (!avahi_service_resolver_new(
-                client, interface, protocol,
-                name, type, domain,
-                AVAHI_PROTO_UNSPEC, 0,
-                resolve_callback, NULL)) {
-
-            printf("%s ERROR: resolver creation failed: %s\n",
-                   ts, avahi_strerror(avahi_client_errno(client)));
-        }
+        avahi_service_resolver_new(
+            client, interface, protocol,
+            name, type, domain,
+            AVAHI_PROTO_UNSPEC, 0,
+            resolve_callback, NULL);
         break;
 
     case AVAHI_BROWSER_REMOVE:
@@ -189,109 +203,75 @@ static void browse_callback(
                ts, name, type, domain);
         break;
 
-    case AVAHI_BROWSER_CACHE_EXHAUSTED:
-        printf("%s CACHE_EXHAUSTED\n", ts);
-        break;
-
-    case AVAHI_BROWSER_ALL_FOR_NOW:
-        printf("%s ALL_FOR_NOW\n", ts);
-        break;
-
-    case AVAHI_BROWSER_FAILURE:
-        fprintf(stderr, "%s BROWSER FAILURE: %s\n",
-                ts, avahi_strerror(avahi_client_errno(client)));
-        avahi_simple_poll_quit(simple_poll);
-        break;
-
     default:
-        printf("%s UNKNOWN_EVENT %d\n", ts, event);
         break;
     }
 }
 
-/* Client state callback */
-static void client_callback(AvahiClient *c, AvahiClientState state, void *userdata)
-{
+/* --------------------------------------------------------- */
+/* Client callback                                           */
+/* --------------------------------------------------------- */
+static void client_callback(AvahiClient *c, AvahiClientState state, void *userdata) {
+    (void)c;
     (void)userdata;
-    client = c;
-
     char ts[16];
     timestamp_now(ts, sizeof(ts));
 
     switch (state) {
-    case AVAHI_CLIENT_S_RUNNING:
-        printf("%s Avahi client running.\n", ts);
-        break;
-
-    case AVAHI_CLIENT_S_REGISTERING:
-    case AVAHI_CLIENT_S_COLLISION:
-        printf("%s Avahi registering/collision.\n", ts);
-        break;
-
     case AVAHI_CLIENT_CONNECTING:
-        printf("%s Avahi connecting.\n", ts);
+        printf("%s CLIENT: connecting\n", ts);
         break;
-
     case AVAHI_CLIENT_FAILURE:
-        fprintf(stderr, "%s Avahi client failure: %s\n",
-                ts, avahi_strerror(avahi_client_errno(client)));
-        avahi_simple_poll_quit(simple_poll);
+        printf("%s CLIENT-ERROR\n", ts);
         break;
-
     default:
-        printf("%s Avahi client state=%d\n", ts, state);
         break;
     }
 }
 
-int main(int argc, char *argv[])
-{
+/* --------------------------------------------------------- */
+/* Main                                                      */
+/* --------------------------------------------------------- */
+int main(int argc, char *argv[]) {
     if (argc < 3) {
-        fprintf(stderr,
-            "Usage: %s <service-type> <domain>\n"
-            "Example: %s _http._tcp local\n",
-            argv[0], argv[0]);
+        fprintf(stderr, "Usage: %s <service_type> <domain>\n", argv[0]);
         return 1;
     }
 
-    const char *service_type = argv[1];
+    const char *stype = argv[1];
     const char *domain = argv[2];
 
-    int error;
-    AvahiServiceBrowser *sb = NULL;
-
-    simple_poll = avahi_simple_poll_new();
-    if (!simple_poll) {
+    AvahiSimplePoll *poll = avahi_simple_poll_new();
+    if (!poll) {
         fprintf(stderr, "Failed to create simple poll\n");
         return 1;
     }
 
-    client = avahi_client_new(
-        avahi_simple_poll_get(simple_poll),
-        0, client_callback, NULL, &error);
+    AvahiClient *client = avahi_client_new(
+        avahi_simple_poll_get(poll),
+        0, client_callback, NULL, NULL);
 
     if (!client) {
-        fprintf(stderr, "Failed to create Avahi client: %s\n",
-                avahi_strerror(error));
+        fprintf(stderr, "Failed to create Avahi client\n");
         return 1;
     }
 
-    sb = avahi_service_browser_new(
-        client, AVAHI_IF_UNSPEC, AVAHI_PROTO_UNSPEC,
-        service_type, domain, 0,
-        browse_callback, NULL);
+    AvahiServiceBrowser *browser = avahi_service_browser_new(
+        client,
+        AVAHI_IF_UNSPEC,
+        AVAHI_PROTO_UNSPEC,
+        stype,
+        domain,
+        0,
+        browse_callback,
+        client);
 
-    if (!sb) {
-        fprintf(stderr, "Failed to create service browser: %s\n",
-                avahi_strerror(avahi_client_errno(client)));
+    if (!browser) {
+        fprintf(stderr, "Failed to create Avahi browser\n");
         return 1;
     }
 
-    avahi_simple_poll_loop(simple_poll);
-
-    avahi_service_browser_free(sb);
-    avahi_client_free(client);
-    avahi_simple_poll_free(simple_poll);
+    avahi_simple_poll_loop(poll);
 
     return 0;
 }
