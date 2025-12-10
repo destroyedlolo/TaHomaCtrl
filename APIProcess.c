582e21a65d7df8b2f522ddc2e2ec559531dd9522
destroyedlolo <l_faillie@yahoo.com>
2025-12-11 00:34:27 +0100

    Parse Gateway ID

--- APIProcess.c
+++ APIProcess.c
@@ -13,6 +13,27 @@ void func_Tgw(const char *){
 	if(debug)
 		printf("*D* Resp: '%s'\n", buff.memory ? buff.memory : "NULL data");
 
+		/* Display result */
+	if(buff.memory){
+		struct json_object *parsed_json = json_tokener_parse(buff.memory);
+		struct json_object *obj;
+
+		if(json_object_is_type(parsed_json, json_type_array)){	/* 1st object is an array */
+			struct json_object *first_object = json_object_array_get_idx(parsed_json, 0);
+			if(first_object && json_object_is_type(first_object, json_type_object)){
+				if(json_object_object_get_ex(first_object, "gatewayId", &obj)){
+					if(json_object_is_type(obj, json_type_string))
+						printf("gatewayId : %s\n", json_object_get_string(obj));
+				} else
+					fputs("*E* gatewayId not found ", stderr);
+			} else
+				fputs("*E* Empty or unexpected object returned", stderr);
+		} else
+			fputs("*E* Returned object is not an array", stderr);
+
+		json_object_put(parsed_json);
+	}
+	
 	freeResponse(&buff);
 }
 
